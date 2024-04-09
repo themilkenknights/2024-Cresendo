5bea783dea3c9042545ea7fb5148db4b234f816d
qrAIhttps <ilanzakai@icloud.com>
2024-04-08 18:45:04 -0700

    logging

--- Cresendo/src/main/java/frc/robot/Robot.java
+++ Cresendo/src/main/java/frc/robot/Robot.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -14,6 +15,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
+    DataLogManager.logNetworkTables(true);
+    DataLogManager.start();
     m_robotContainer = new RobotContainer();
   }
 
